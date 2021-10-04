499c32da56f030a6d5babb5857cc5055e3decbb4
LuminiNanayakkara <lumee2000@gmail.com>
2021-10-04 10:53:23 +0530

    Implement 404 error in server.js file

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const app=express();
+const app = express();
+const HttpError = require("./models/http-error");
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(3000);
 
-app.get('/',(req,res) => {
-    res.send('<p>Home Page</p>')
-})
+//404 Error
+app.use((req, res, next) => {
+  const error = new HttpError("could not find this route", 404);
+  throw error;
+});
+
+app.get("/", (req, res) => {
+  res.send("<p>Home Page</p>");
+});
